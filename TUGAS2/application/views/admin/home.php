955d98e5cb59d96c160dffac89fb499b44128714
Oktisya02781 <81371391+Oktisya02781@users.noreply.github.com>
2021-04-22 16:14:19 +0700

    soal234

---
+++ TUGAS2/application/views/admin/home.php
@@ -0,0 +1,29 @@
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+<br>
+<br>
+<br>
+
+ <center>
+ 	<h3><p>
+ 		WELCOME TO HALAMAN ADMIN!
+ 	</p> </h3>
+ </center>
+ 		
+
+<br>
+<br>
+<br>
+<br>
+
+
+ 		 <center>
+ 	<p>
+ 		HALLO ADMIN PERPUSTAKAAN OKTISYA!
+ 	</p>
+ </center>
